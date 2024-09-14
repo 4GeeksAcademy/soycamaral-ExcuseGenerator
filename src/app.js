adcd41d4b74b9c7c2248a5352d012da62db91144
soycamaral <camaralr95@gmail.com>
2024-09-14 23:12:43 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,45 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let who = ["Mi profesor", "Mi suegra", "El vecino", "Mi perro", "Mi ex"];
+
+let action = ["aplastó", "quebró", "rompió", "robó"];
+
+let what = [
+  "mi computadora",
+  "mi celular",
+  "mi billetera",
+  "mi mate",
+  "mi comida",
+  "a mi novio"
+];
+
+let when = [
+  "camino al trabajo",
+  "mientras dormía",
+  "en la discoteca",
+  "mientras me bañaba",
+  "mientras programaba"
+];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+function getRandom(anyArray) {
+  let max = who.length - 1;
+  let min = 0;
+  let Random = getRandomNumber(min, max);
+  return anyArray[Random];
+}
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
